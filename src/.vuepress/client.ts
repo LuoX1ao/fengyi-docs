1d718da3e4ca4fee88c571a0c82268165412cd81
罗潇 <luoxiao11@sf-express.com>
2024-12-24 10:50:27 +0800

    feat: 浏览器网页名称维持“丰翼开放平台”的字样

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -13,7 +13,7 @@ export default defineClientConfig({
 
     router.beforeEach((to, from, next) => {
       console.log(`全局守卫：从 ${from.path} 到 ${to.path}`);
-      // 可以在这里触发公共组件的逻辑
+      document.title = '丰翼开放平台'
       next(); // 必须调用 next() 继续导航
     });
   },
