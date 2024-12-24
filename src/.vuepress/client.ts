58f2b88526c037ba72b12a0213caf664d49dc3db
罗潇 <luoxiao11@sf-express.com>
2024-12-24 11:33:59 +0800

    。

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -13,9 +13,6 @@ export default defineClientConfig({
 
     router.beforeEach((to, from, next) => {
       console.log(`全局守卫：从 ${from.path} 到 ${to.path}`);
-      if (typeof window === 'object') {
-        document.title = '丰翼开放平台'
-      }
       next(); // 必须调用 next() 继续导航
     });
   },
